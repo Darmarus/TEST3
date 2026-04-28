b73ed4b16eb94c8760127daed315a711bd053a19
Robert Osiński <robert.osinski@agemit.pl>
2026-04-28 14:24:01 +0200

    .test

---
+++ TEST/TEST/MyWorker1Worker.cs
@@ -0,0 +1,32 @@
+﻿using Soneta.Business;
+using Soneta.Business.UI;
+using Soneta.Handel;
+using System;
+using TEST;
+
+[assembly: Worker(typeof(MyWorker1Worker), typeof(DokHandlowe))]
+
+namespace TEST
+{
+	public class MyWorker1Worker
+	{
+
+
+		// TODO -> Należy podmienić podany opis akcji na bardziej czytelny dla uzytkownika
+		[Action("MyWorker1Worker/ToDo", Mode = ActionMode.SingleSession | ActionMode.ConfirmSave | ActionMode.Progress)]
+		public MessageBoxInformation ToDo()
+		{
+
+			return new MessageBoxInformation("Czy wykonać operację ?")
+			{
+				Text = "Opis operacji",
+				YesHandler = () => "Operacja została zakończona",
+				NoHandler = () => "Operacja przerwana"
+			};
+
+
+		}
+	}
+
+
+}
